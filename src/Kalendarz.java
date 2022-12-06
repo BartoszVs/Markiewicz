45858a808b656cf6a35fcfc6c79447d44d1f58bb
Klasa2pp <bartek.vs@o2.pl>
2022-12-06 12:27:21 +0100

    Pliki z lekcji

---
+++ src/Kalendarz.java
@@ -0,0 +1,12 @@
+public class Kalendarz {
+    public static void main(String[] args) {
+        System.out.println("*********************");
+        System.out.println("*                   *");
+        System.out.println("*         6         *");
+        System.out.println("*      Grudnia      *");
+        System.out.println("*       2022        *");
+        System.out.println("*     Mikołajki     *");
+        System.out.println("*                   *");
+        System.out.println("*********************");
+    }
+}
---
+++ src/Wizytowka.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+
+public class Wizytowka {
+    public static void main(String[] args) {
+        System.out.println("******************************************************");
+        System.out.println("*                    Bartosz Markiewicz              *");
+        System.out.println("*                             Klasa 2pp              *");
+        System.out.println("******************************************************");
+    }
+}
